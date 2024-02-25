07e8c03d410dc9c61982c958573cd1b68c80776a
Oscar I. Rojas Hernandez <oscar.rojas991@gmail.com>
2024-02-25 00:31:52 -0600

    Updated nvim plugins and polybar

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,14 +1,14 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", 
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim", 
-    },
-    config = function()
-        require("neo-tree").setup({})
-	      vim.keymap.set("n","<leader>e",":Neotree toggle<cr>", {})	
-    end
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({})
+    vim.keymap.set("n", "<leader>fe", ":Neotree toggle<cr>", {})
+  end,
 }
